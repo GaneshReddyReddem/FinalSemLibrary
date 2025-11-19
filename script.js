a21dd9bce8fe43d2de086dc5b827b44d2ec8adad
GaneshReddyReddem <158313866+GaneshReddyReddem@users.noreply.github.com>
2025-11-19 17:46:46 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("click",function(event){
+    if(event .target.tagName == "a")
+    event.preventDefault();
+    alert("This website is meant for my learning practice ! ")
+})
